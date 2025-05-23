cd329bb37f6660e4028228b42f763e9dd583b908
nurulislamrimon <nurulislamrimon@gmail.com>
2025-05-23 22:27:58 +0600

    EXPRESSION/DONE

--- script.js
+++ script.js
@@ -16,13 +16,20 @@ function startVideo() {
         err => console.log("Error -------------- ", err)
     )
 }
-
+// add expressions
 video.addEventListener("play", () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    // detect expression
     setInterval(async () => {
-
         const detections = await faceapi.detectAllFaces(video,
             new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-        console.log(detections)
-
-    }, 10000)
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    }, 100)
 })
