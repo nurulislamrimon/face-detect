6e578514388a892aebf5f56d10dd28e1e49dc4f2
nurulislamrimon <nurulislamrimon@gmail.com>
2025-05-23 22:07:08 +0600

    [FACE_DETECTION_MODELS/ADDED]: face detection implimeimented

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const video = document.getElementById("video")
 
+// load models
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]).then(startVideo)
 
+// start video and render
 function startVideo() {
     navigator.getUserMedia(
         { video: {} }
@@ -9,4 +17,12 @@ function startVideo() {
     )
 }
 
-startVideo()
+video.addEventListener("play", () => {
+    setInterval(async () => {
+
+        const detections = await faceapi.detectAllFaces(video,
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        console.log(detections)
+
+    }, 10000)
+})
